14bc34c87d92666adb30363cc34c3f275f686da2
freedisch <freeproduc@gmail.com>
2024-04-29 21:16:40 +0200

    updated run.sh
    Signed-off-by: freedisch <freeproduc@gmail.com>

--- run.sh
+++ run.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-echo "Starting script execution..."
-# Add a command to log memory usage, for example using `free` or `top`:
-free -h
-echo "Memory usage logged."
-
-# Your existing commands here
-# Example:
-python3 main.py
-
-echo "Script completed."
+pip install -r requirements.txt
+python main.py
